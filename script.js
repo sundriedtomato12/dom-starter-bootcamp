3359158b4d6e073179969d41463f2c072f294abb
root <root@DESKTOP-P0PA6DP.localdomain>
2021-11-18 15:34:42 +0800

    created nested loops for nested array! made 6 grey boxes, with two yellow rows each (one for each item type), and each word in their own pink box.

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 // Please implement exercise logic here
 // eslint-disable-next-line no-plusplus
 
-const words = ['banana', 'pasta', 'alligator', 'purple', 'accurate', 'fickle'];
-
 for (let i = 0; i < 6; i++) {
   const container = document.createElement('div');
   document.body.appendChild(container).className = 'container';
 
-  const row = document.createElement('div');
-  container.appendChild(row).className = 'row';
+  for (let c = 0; c < 2; c++) {
+    const row = document.createElement('div');
+    container.appendChild(row).className = 'row';
 
-  for (let c = 0; c < 6; c++) {
-    const wordBox = document.createElement('span');
-    wordBox.innerText = words[c];
-    row.appendChild(wordBox).className = 'word';
+    for (let e = 0; e < 2; e++) {
+      const words = [
+        ['orange', 'tomato'],
+        ['fire engine', 'basketball'],
+      ];
+      const wordBox = document.createElement('span');
+      wordBox.innerText = words[c][e];
+      row.appendChild(wordBox).className = 'word';
+    }
   }
 }
