90d3b4ba96e0a0b15e565cd9a9db339d94d2f4fc
root <root@DESKTOP-P0PA6DP.localdomain>
2021-11-18 14:23:37 +0800

    created loop to create grey boxes with yellow rows and pink boxes with the index in it

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // Please implement exercise logic here
+// create container (div)
+
+for (let i = 1; i < 7; i++) {
+  const container = document.createElement('div');
+  document.body.appendChild(container).className = 'container';
+
+  const row = document.createElement('div');
+  container.appendChild(row).className = 'row';
+
+  const counter = document.createElement('span');
+  counter.innerText = i;
+  row.appendChild(counter).className = 'word';
+}
